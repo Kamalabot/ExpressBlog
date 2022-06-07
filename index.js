51632bf84d1efdadff8b7e7868528e03b153182e
Kamalraj M M <kamaljp@gmail.com>
2022-06-07 08:25:06 +0530

    Entering the UserAuth flow

--- index.js
+++ index.js
@@ -27,10 +27,14 @@ app.get('/',(req, res) =>{
         name:'Local Love'
     })
 })
-*/
+
 app.get('/post/new',(req,res) =>{
     res.render('create');
 })
+*/
+
+const newCreateController = require('./controllers/newPost');
+app.get('/post/new',newCreateController);
 
 app.post('/post/store',(req,res) =>{
     let image = req.files.image;
@@ -38,17 +42,28 @@ app.post('/post/store',(req,res) =>{
         await BlogPost.create(req.body)
         res.redirect('/');
     })
-})
+})  
+
+const newAboutController = require('./controllers/about');
+app.get('/about',newAboutController);
+/*
 app.get('/about',(req, res) =>{
     //res.sendFile(path.resolve(__dirname,'pages/about.html'));
     res.render('about');
 })
+*/
 
+const newContactController = require('./controllers/contact');
+app.get('/contact', newContactController);
+/*
 app.get('/contact',(req, res) =>{
     //res.sendFile(path.resolve(__dirname,'pages/contact.html'));
     res.render('contact');
 })
-
+*/
+const newHomeController = require('./controllers/index');
+app.get('/',newHomeController);
+/*
 app.get('/', async(req, res) =>{
     //res.sendFile(path.resolve(__dirname,'pages/index.html'));
     const blogposts = await BlogPost.find({});
@@ -57,10 +72,13 @@ app.get('/', async(req, res) =>{
     });
 })
 //console.log(bloglist)
-app.get('/post/:id',async(req, res) =>{
+*/
+const newPostController = require('./controllers/post');
+app.get('/post',newPostController);
+/*
+app.get('/post',async(req, res) =>{
     //res.sendFile(path.resolve(__dirname,'pages/post.html'));
-    const blogpost = await BlogPost.findById(req.params.id);
-    res.render('post',{
-        blogpost
-    });
+    //const blogpost = await BlogPost.findById(req.params.id);
+    res.render('post');
 })
+*/
